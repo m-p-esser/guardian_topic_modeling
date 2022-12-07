296622fb6dd756ba380e78cf376d1e0616d7577a
Marc-Philipp Esser <m-esser@mail.de>
2022-12-07 16:04:26 +0100

    Clean yearly partioned data and create one Parquet file per year

--- src/pipeline/clean.py
+++ src/pipeline/clean.py
@@ -34,67 +34,73 @@ def clean(config_file):
     # Load Config from Config File
     logger.info(f"Load config from {config_file}")
     config = parse_config(config_file)
-    logger.info(f"config: {config['clean']} parsed")
+    logger.info(f"config: {config} parsed")
+
+    ############ Do the steps below for all Metadata Files (all Years)
 
     # Load Metadata
-    file_path = pathlib.Path(config["clean"]["interim_file_path"]) / "metadata.parquet"
-    df = pd.read_parquet(file_path)
+    file_paths = pathlib.Path(config["combine"]["interim_file_path"]).rglob("*_metadata.parquet")
+    for fp in file_paths:
+        year = fp.stem[0:4]
+        df = pd.read_parquet(fp)
 
-    # Only keep english Articles
-    only_articles = df["type"] == "article"
-    only_english = df["fields_lang"] == "en"
-    df = df[only_english & only_articles]
+        # Only keep english Articles
+        only_articles = df["type"] == "article"
+        only_english = df["fields_lang"] == "en"
+        df = df[only_english & only_articles]
 
-    # Remove prefix from column names
-    df.columns = [col.replace("fields_", "") for col in df.columns]
+        # Remove prefix from column names
+        df.columns = [col.replace("fields_", "") for col in df.columns]
 
-    # Remove irrelevant columns
-    drop_columns = [
-        "sectionId",
-        "apiUrl",
-        "pillarId",
-        "main",
-        "bylineHtml",
-        "thumbnail",
-        "displayHint",
-    ]
-    keep_columns = [col for col in df.columns if col not in drop_columns]
-    df = df.loc[:, keep_columns]
+        # Remove irrelevant columns
+        drop_columns = [
+            "sectionId",
+            "apiUrl",
+            "pillarId",
+            "main",
+            "bylineHtml",
+            "thumbnail",
+            "displayHint",
+        ]
+        keep_columns = [col for col in df.columns if col not in drop_columns]
+        df = df.loc[:, keep_columns]
 
-    # Convert Datetime formatted as String to Date
-    for col in [
-        "webPublicationDate",
-        "fields_lastModified",
-        "fields_newspaperEditionDate",
-        "fields_firstPublicationDate",
-        "fields_scheduledPublicationDate",
-    ]:
-        df[col] = pd.to_datetime(df[col])
+        # Convert Datetime formatted as String to Date
+        for col in [
+            "webPublicationDate",
+            "fields_lastModified",
+            "fields_newspaperEditionDate",
+            "fields_firstPublicationDate",
+            "fields_scheduledPublicationDate",
+        ]:
+            if col in df.columns:
+                df[col] = pd.to_datetime(df[col])
 
-    # Convert Object to Categorical Variables
-    df[df.select_dtypes(["object"]).columns] = df.select_dtypes(["object"]).apply(
-        lambda x: x.astype("category")
-    )
+        # Convert Object to Categorical Variables
+        df[df.select_dtypes(["object"]).columns] = df.select_dtypes(["object"]).apply(
+            lambda x: x.astype("category")
+        )
 
-    # Recode variables
-    for col in [
-        "showAffiliateLinks",
-        "isInappropriateForSponsorship",
-        "sensitive",
-        "commentable",
-    ]:
-        df[col] = df[col].map({0: False, 1: True})
+        # Recode variables
+        for col in [
+            "showAffiliateLinks",
+            "isInappropriateForSponsorship",
+            "sensitive",
+            "commentable",
+        ]:
+            if col in df.columns:
+                df[col] = df[col].map({0: False, 1: True})
 
-    # Remove HTML Tags
-    for col in ["standfirst", "trailText"]:
-        df[col] = df[col].apply(lambda x: remove_html(str(x)))
+        # Remove HTML Tags
+        for col in ["standfirst", "trailText"]:
+            df[col] = df[col].apply(lambda x: remove_html(str(x)))
 
-    # Store locally
-    output_file_path = (
-        pathlib.Path(config["clean"]["cleaned_file_path"]) / "metadata_cleaned.parquet"
-    )
-    df.to_parquet(output_file_path)
-    logger.info(f"Cleaned metadata and stored here: {output_file_path}")
+        # Store locally
+        output_file_path = (
+                pathlib.Path(config["clean"]["cleaned_file_path"]) / f"{year}_metadata_cleaned.parquet"
+        )
+        df.to_parquet(output_file_path)
+        logger.info(f"Cleaned metadata and stored here: {output_file_path}")
 
 
 if __name__ == "__main__":
